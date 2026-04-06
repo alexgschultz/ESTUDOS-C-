df1fedfcb892b23cc4443ecd66fa609889e0e859
AlexXMano <alexgomesschultz@gmail.com>
2026-04-06 11:59:10 -0300

    Nova aula de variáveis

---
+++ C++/Declarações multiplas de variáveis.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.14159
+#define canal cout << "Canal: Ir.Shadow" << endl;
+
+int main()
+{
+	/* int vidas;
+	   int tiros;
+	   int life; */
+
+	int vidas = 3, tiros = 500, life = 100;
+
+	cout << PI << endl;
+	canal
+
+	return 0;
+}

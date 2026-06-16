d99e6888afc0074d2c47b72fb97268a714c05561
AlexXMano <alexgomesschultz@gmail.com>
2026-06-16 13:31:29 -0300

    Comando system

---
+++ C++/System.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{   
+    system("color 0f");// Altera a cor do prompt de comando
+    system("cls");// Comando cls limpa a tela
+    
+    cout << ("Ola Mundo") << endl;
+
+    system("pause");// O comando "pause" pausa o prompt e espera uma tecla ser pressionada
+
+    return 0;
+}
